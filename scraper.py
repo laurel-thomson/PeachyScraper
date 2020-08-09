30da68f241ee59cb71cfec13b88f530a8d6320e7
laurel-thomson <laurel228@ksu.edu>
2020-08-09 13:46:28 -0700

    Getting all minimalist baker pages

--- scraper.py
+++ scraper.py
@@ -2,11 +2,13 @@ import scrapy
 
 class RecipeSpider(scrapy.Spider):
 	name = 'recipespider'
-	start_urls = [ 'https://minimalistbaker.com/recipe-index/?fwp_special-diet=vegan' ]
+	start_urls = [ 'https://minimalistbaker.com/recipe-index?fwp_special-diet=vegan&fwp_paged=57' ]
 
 	def parse(self, response):
 		for recipeLink in response.css('.post-summary__title'):
 			yield { 'link' : recipeLink.css('a::attr(href)').get() }
-		# TODO: go to recipe detail pages
 		
-		# TODO: go to next page
+		if len(response.css('.post-summary__title')) > 0:
+			next_page_number = int(response.url.split('fwp_paged=')[1]) +  1
+			next_page = f'https://minimalistbaker.com/recipe-index?fwp_special-diet=vegan&fwp_paged={next_page_number}'
+			yield response.follow(next_page, callback=self.parse)
